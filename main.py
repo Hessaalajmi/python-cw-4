6f4c199bf51ecf5bce7ab08ea07e8a49997f9061
Hessaalajmi <142667801+Hessaalajmi@users.noreply.github.com>
2023-09-02 19:32:30 +0300

    cw-4

---
+++ main.py
@@ -0,0 +1,23 @@
+def my_name():
+    print("My name is Hessa")
+my_name()
+
+def my_meal(food, drink):
+    print('I like to eat {food} while drinking {drink}')
+my_meal('sandwich', 'water')
+
+def cube(number):
+    return number**3 
+
+
+def by_three(number):
+    if number%3==0:
+        return cube(number)
+    else:
+        return False           
+
+x = cube(7)
+print(x)
+
+y = by_three(7)
+print(y)
